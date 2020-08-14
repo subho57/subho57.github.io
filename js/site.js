3f7f638c3c1aebcae1c6acd4d596a6274bd86204
Subhankar Pal <subho.playboy10@gmail.com>
2020-08-14 19:06:54 +0530

    ki jani ki korlam :-(

--- js/site.js
+++ js/site.js
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-navigator.serviceWorker && navigator.serviceWorker.register('sw.js').then(function(registration)
+navigator.serviceWorker && navigator.serviceWorker.register('js/sw.js').then(function(registration)
 {	console.log('Excellent, registered with scope: ', registration.scope);
 });
